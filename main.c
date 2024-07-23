9520712278989bc5dc91faa858fe93852424e746
iancapon <89599951+iancapon@users.noreply.github.com>
2024-07-23 14:53:27 -0300

    ahora muestra el valor en binario de la memoria

--- main.c
+++ main.c
@@ -51,29 +51,31 @@ uint8_t FL= 0x00;// FLAGS_ REGISTER
 
 uint8_t * memory;//arreglo de la memoria
 
-void printMemory(){
-    printf("REG_A: %3d | REG_B: %3d | FLAGS: %3d | OUTPUT: %3d | P_COUNTER: %2x \n\n",AR,BR,FL,OR,PC);
-    for(int i=0; i< 16; i++){
-        if(i==PC){
-            printf(">  %2x: ",i);
-        }else{
-            printf("   %2x: ",i);
-        }
-        if((memory[i]>>4) < 11){
-            printf("%s $%x ",OPS[(memory[i]>>4)],memory[i]&0b00001111);
-        }else{
-            printf("$%5x ",memory[i]);
-        }
-        printf("| %4d\n",memory[i]);
-    }
-    printf("\n\n");
-}
+void printMemory();
+void appendChar(char*text,int*pointer,char c);
+char* cargarDesdeArchivo();
+uint8_t matchOp(char*word);
+int hexToDecChar(char c);
+int hexToDecStr(char*word);
+void parsearTexto(char*input);
+void run(uint16_t*clk);
+char*decToBinary(uint8_t input);
 
-void appendChar(char*text,int*pointer,char c){
-    text=realloc(text,sizeof(char)*(*pointer+2));
-    text[*pointer+1]=0;
-    text[*pointer]=c;
-    *pointer+=1;
+int main(){
+    uint16_t clock_pulse=0;
+    memory=(uint8_t*)malloc(sizeof(uint8_t)*16);
+    char*text=cargarDesdeArchivo();    
+    parsearTexto(text);
+    free(text);
+    char c='\n';
+    while(c=='\n'){
+        printf("\n CLK: %d\n",clock_pulse);
+        printMemory();
+        run(&clock_pulse);
+        c=getchar();
+        clock_pulse++;
+    }
+    return 0;
 }
 
 char* cargarDesdeArchivo(){
@@ -96,40 +98,6 @@ char* cargarDesdeArchivo(){
     }
     fclose(file);
     return input;
-}
-
-uint8_t matchOp(char*word){
-    uint8_t i=0;
-    while (i<11){
-        if(strcmp(OPS[i],word)==0){
-            return i;
-        }
-        i++;
-    }
-    return 255;
-}
-
-int hexToDecChar(char c){
-    int ret =(int)c;
-    if(ret>=48 && ret<=57){
-        ret-=48;
-    }else if(ret>=97 && ret<=102){
-        ret-=87;
-    }
-    //printf(" DEBUG:%d\n",ret);
-    return ret;
-}
-
-int hexToDecStr(char*word){
-    int i=strlen(word)-1;
-    int mult=1;
-    int ret=0;
-    while (i>0){
-        ret+=hexToDecChar(word[i])*mult;
-        mult*=16;
-        i--;
-    }
-    return ret;
 }
 
 void parsearTexto(char*input){
@@ -172,6 +140,24 @@ void parsearTexto(char*input){
     free(word);
 }
 
+void printMemory(){
+    printf("REG_A: %3d | REG_B: %3d | FLAGS: %3d | OUTPUT: %3d | P_COUNTER: %2x \n\n",AR,BR,FL,OR,PC);
+    for(int i=0; i< 16; i++){
+        if(i==PC){
+            printf(">  %2x: ",i);
+        }else{
+            printf("   %2x: ",i);
+        }
+        if((memory[i]>>4) < 11){
+            printf("%s $%x ",OPS[(memory[i]>>4)],memory[i]&0b00001111);
+        }else{
+            printf("$%5x ",memory[i]);
+        }
+        printf("| %4d |",memory[i]);
+        printf("%15s\n",decToBinary(memory[i]));
+    }
+    printf("\n\n");
+}
 
 void run(uint16_t*clk){
     uint8_t x=memory[PC]&0b11110000;
@@ -239,77 +225,64 @@ void run(uint16_t*clk){
     *clk+=3;
 }
 
-int main(){
-    uint16_t clock_pulse=0;
-    memory=(uint8_t*)malloc(sizeof(uint8_t)*16);
-    char*text=cargarDesdeArchivo();    
-    parsearTexto(text);
-    //printMemory();
-    free(text);
-    char c='\n';
-    while(c=='\n'){
-        printf("\n CLK: %d\n",clock_pulse);
-        printMemory();
-        run(&clock_pulse);
-        c=getchar();
-        clock_pulse++;
+char*decToBinary(uint8_t input){
+    char *output=malloc(sizeof(char)*10);
+    output[9]=0;
+    int i=8;
+    while (i>=0){
+        if(i==4){
+            output[i]=' ';
+        }
+        else {
+            output[i]='1';
+            if(input%2==0){
+                output[i]='0';
+            }
+            input=input/2;
+        }
+        i--;
     }
-    return 0;
+    return output;
 }
 
-/*void parsearTexto(char*input){
-    int i=0;
-    uint8_t pc=0;
-    uint8_t op=0;
-    ///
-    char*word=malloc(sizeof(char));
-    int p=0;
-    word[p]=0;
-    ///
-    do{
-        if(input[i]!=' ' && input[i]!='\n'){
-            appendChar(word,&p,input[i]);
-        }
-        else{
-            //printf("%s - ",word);
-            if(word[1]==':'){/// linea n°
-                if(word[0]<=57 && word[0]>=48){
-                    pc=word[0]-48;
-                }else if(word[0]<=102 && word[0]>=97){
-                    pc=word[0]-97;
-                }
-            op=0;
-            printf("%d: ",pc);
-            }
-            else if(matchOp(word)!=255){
-                op+=((0+matchOp(word))<<4)&0b11110000;
-                printf("%s ",OPS[matchOp(word)]);
-                printf("%d :: ",matchOp(word)<<4);
-            }
-            else if(word[0]=='$'){
-                char aux[4]={0};
-                int j=1;
-                while(word[j]!=0){
-                    aux[j-1]=word[j];
-                    j++;
-                }
-                //printf("%d\n",atoi(aux));
-                op+=atoi(aux);
-                
-            }else{
-                //printf("%d\n",atoi(word));
-                op+=atoi(word);
-                printf("%d ",op);
-                p=0;
-                memory[pc]=op;
-                op=0;
-            }
-            //printf("%d\n",op);
-            p=0;
-            
+void appendChar(char*text,int*pointer,char c){
+    text=realloc(text,sizeof(char)*(*pointer+2));
+    text[*pointer+1]=0;
+    text[*pointer]=c;
+    *pointer+=1;
+}
+
+uint8_t matchOp(char*word){
+    uint8_t i=0;
+    while (i<11){
+        if(strcmp(OPS[i],word)==0){
+            return i;
         }
         i++;
-    } while(input[i]!=0);
+    }
+    return 255;
+}
 
-    free(word);
-}*/
+int hexToDecChar(char c){
+    int ret =(int)c;
+    if(ret>=48 && ret<=57){
+        ret-=48;
+    }else if(ret>=97 && ret<=102){
+        ret-=87;
+    }
+    //printf(" DEBUG:%d\n",ret);
+    return ret;
+}
+
+int hexToDecStr(char*word){
+    int i=strlen(word)-1;
+    int mult=1;
+    int ret=0;
+    while (i>0){
+        ret+=hexToDecChar(word[i])*mult;
+        mult*=16;
+        i--;
+    }
+    return ret;
+}
+
